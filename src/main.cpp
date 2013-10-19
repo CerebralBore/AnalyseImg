f5e0488cf3a7122e017f1435ee129b6603f5467a
Kevin_Lemee <krigorn@ubuntu.(none)>
2013-10-19 20:24:30 +0200

    gradient ok

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,62 @@
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 #include <vector>
+#include <math.h>
 #include <string>
 
 using namespace std;
 
 string demanderImage();
 
-cv::Mat gris(cv::Mat*);
-cv::Mat filtreImage(cv::Mat*, cv::Mat*);
+void calculGradient(cv::Mat& img);
+cv::Mat norme(cv::Mat img);
+cv::Mat applyFilter(cv::Mat& img, cv::Mat& filtre);
+
+std::vector<cv::Mat> prewittFilter, sobelFilter, kirschFilter, usedFilter;
+std::vector<cv::Mat> usedFilteredImg;
+cv::Mat module;
+cv::Mat pente;
 
 int main()
 {
     string cheminRepImg, cheminImage;
-    cv::Mat imgO, imgGris, imgFlou;
+    cv::Mat imgO;
     bool bonChemin;
 
-    double m1[3][3] = {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
-    cv::Mat flou = cv::Mat(3, 3, CV_64F, m1);
-    double m2[3][3] = {{-1, 0, 1}, {-1, 0, 1}, {-1, 0, 1}};
-    cv::Mat prewitt = cv::Mat(3, 3, CV_64F, m2);
-    double m3[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
-    cv::Mat sobeL = cv::Mat(3, 3, CV_64F, m3);
-    double m4[3][3] = {{-3, -3, 5}, {-3, 0, 5}, {-3, -3, 5}};
-    cv::Mat kirsch = cv::Mat(3, 3, CV_64F, m4);
+    double m2_1[3][3] = {{-1, 0, 1}, {-1, 0, 1}, {-1, 0, 1}};
+    double m2_2[3][3] = {{1, 1, 1}, {0, 0, 0}, {-1, -1,-1}};
+    double m2_3[3][3] = {{1, 1, 0}, {1, 0, -1}, {0, -1, -1}};
+    double m2_4[3][3] = {{0, 1, 1}, {-1, 0, 1}, {-1, -1, 0}};
+
+    prewittFilter.push_back(cv::Mat(3, 3, CV_64FC1, m2_1));
+    prewittFilter.push_back(cv::Mat(3, 3, CV_64FC1, m2_2));
+    prewittFilter.push_back(cv::Mat(3, 3, CV_64FC1, m2_3));
+    prewittFilter.push_back(cv::Mat(3, 3, CV_64FC1, m2_4));
+
+    double m3_1[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+    double m3_2[3][3] = {{1, 2, 1}, {0, 0, 0}, {-1, -2, -1}};
+    double m3_3[3][3] = {{2, 1, 0}, {1, 0, -1}, {0, -1, -2}};
+    double m3_4[3][3] = {{0, 1, 2}, {-1, 0, 1}, {-2, -1, 0}};
+
+    sobelFilter.push_back(cv::Mat(3, 3, CV_64FC1, m3_1));
+    sobelFilter.push_back(cv::Mat(3, 3, CV_64FC1, m3_2));
+    sobelFilter.push_back(cv::Mat(3, 3, CV_64FC1, m3_3));
+    sobelFilter.push_back(cv::Mat(3, 3, CV_64FC1, m3_4));
+
+    double m4_1[3][3] = {{-3, -3, 5}, {-3, 0, 5}, {-3, -3, 5}};
+    double m4_2[3][3] = {{5, 5, 5}, {-3, 0, -3}, {-3, -3, -3}};
+    double m4_3[3][3] = {{5, 5, -3}, {5, 0, -3}, {-3, -3, -3}};
+    double m4_4[3][3] = {{-3, 5, 5}, {-3, 0, 5}, {-3, -3, -3}};
+
+    kirschFilter.push_back(cv::Mat(3, 3, CV_64FC1, m4_1));
+    kirschFilter.push_back(cv::Mat(3, 3, CV_64FC1, m4_2));
+    kirschFilter.push_back(cv::Mat(3, 3, CV_64FC1, m4_3));
+    kirschFilter.push_back(cv::Mat(3, 3, CV_64FC1, m4_4));
+
+
+    usedFilter = prewittFilter;
+
+
 
     char key = '-';
     while (key != 'q' && key != 'Q')
@@ -36,7 +69,7 @@ int main()
             cheminImage = cheminRepImg;
             cheminImage += demanderImage();
 
-            imgO = cv::imread (cheminImage);
+            imgO = cv::imread (cheminImage, CV_LOAD_IMAGE_GRAYSCALE);
 
             if (imgO.empty ())
                 cerr << "Mauvais chemin d'image: " << cheminImage << endl;
@@ -44,14 +77,12 @@ int main()
                 bonChemin = true;
         }
 
-        imgGris = gris(&imgO);
-        imgFlou = filtreImage(&imgO, &flou);
+        equalizeHist(imgO, imgO);
+        calculGradient(imgO);
 
         cv::imshow("Image original", imgO);
         cvWaitKey();
-        cv::imshow("Image niveau de gris", imgGris);
-        cvWaitKey();
-        cv::imshow("Image flou", imgFlou);
+        cv::imshow("module", norme(module));
 
         cout << "Cliquez sur une fenetre d'OpenCv puis (q) pour pour quitter, (s) pour segmenter une nouvelle image." << endl;
         key = '-';
@@ -63,6 +94,130 @@ int main()
     }
 }
 
+cv::Mat norme(cv::Mat img)
+{
+    cv::Mat img_out(img.rows, img.cols, CV_8UC1);
+
+    // Récupération des valeurs min et max de l'image
+    double min = img.at<double>(0, 0);
+    double max = img.at<double>(0, 0);
+    for (int i = 0; i < img.rows ; i++) {
+        for(int j = 0; j < img.cols ; j++) {
+            if (min > img.at<double>(i, j)) min = img.at<double>(i, j);
+            if (max < img.at<double>(i, j)) max = img.at<double>(i, j);
+        }
+    }
+
+    // Normage de l'image pour avoir des valeurs entre [O, 255]
+    for (int i = 0; i < img.rows ; i++) {
+        for(int j = 0; j < img.cols ; j++) {
+            img_out.at<uchar>(i, j) = round(((img.at<double>(i, j)-min)/(max-min)) * 255);
+            //if(round(((img.at<double>(i, j)-min)/(max-min)) * 255) > 0)std::cout << round(((img.at<double>(i, j)-min)/(max-min)) * 255) << std::endl;
+        }
+    }
+
+    return img_out;
+}
+
+
+#define GRADIENT_MAX 0
+#define GRADIENT_SUM 1
+void calculGradient(cv::Mat& img)
+{
+    int modeCalculGradient = 1;
+    int nbDirection = 2;
+    int angle = 180/nbDirection;
+
+
+    module = cv::Mat(img.rows, img.cols, CV_64FC1);
+    pente = cv::Mat(img.rows, img.cols, CV_64FC1);
+
+    usedFilteredImg.push_back(applyFilter(img, usedFilter[0]));
+    usedFilteredImg.push_back(applyFilter(img, usedFilter[1]));
+    usedFilteredImg.push_back(applyFilter(img, usedFilter[2]));
+    usedFilteredImg.push_back(applyFilter(img, usedFilter[3]));
+
+
+    cv::imshow("usedFilteredImg 0 ", norme(usedFilteredImg[0]));
+    cv::imshow("usedFilteredImg 1 ", norme(usedFilteredImg[1]));
+    cvWaitKey();
+
+    std::cout << "usedFilteredImg[0].rows = " << usedFilteredImg[0].rows << "   usedFilteredImg[0].cols = " << usedFilteredImg[0].cols << std::endl;
+/*
+    for(int i = 0 ; i < img.rows ; i++)
+        for(int j = 0 ; j < img.cols ; j++)
+            if(usedFilteredImg[0].at<double>(i,j) > 1) std::cout << usedFilteredImg[0].at<double>(i,j) << "    ";
+    */
+
+
+    switch(modeCalculGradient)
+    {
+        case GRADIENT_SUM:
+            for(int i = 0 ; i < img.rows ; i++) {
+                for(int j = 0 ; j < img.cols ; j++) {
+                    double ksomme = 0;
+                    int kmax = 0;
+                    for(int k = 0 ; k < nbDirection ; k++) {
+                        ksomme += usedFilteredImg[k].at<double>(i, j);
+                        if(usedFilteredImg[k].at<double>(i, j) > usedFilteredImg[kmax].at<double>(i, j)) {
+                            kmax = k;
+                        }
+                    }
+                    module.at<double>(i, j) = ksomme / nbDirection;
+                    //module.at<double>(i, j) = usedFilteredImg[ksomme].at<double>(i, j);
+
+                    if(nbDirection== 2)
+                    {
+                        double angle = atan( usedFilteredImg[1].at<double>(i, j) / usedFilteredImg[0].at<double>(i, j));
+                        angle *= 180 / M_PI;
+                        //printf("angle %f \n", angle);
+                        pente.at<double>(i, j) = angle;
+                    }
+                    else
+                        pente.at<double>(i, j) = kmax * angle;
+                }
+            }
+            break;
+
+    case GRADIENT_MAX:
+        for(int i = 0 ; i < img.rows ; i++) {
+            for(int j = 0 ; j < img.cols ; j++) {
+                int kmax = 0;
+                for(int k = 0 ; k < nbDirection ; k++) {
+                    if(usedFilteredImg[k].at<double>(i, j) > usedFilteredImg[kmax].at<double>(i, j)) {
+                        kmax = k;
+                    }
+                }
+                module.at<double>(i, j) = usedFilteredImg[kmax].at<double>(i, j);
+
+                if(nbDirection == 2)
+                {
+                    double angle = 90;
+                    if( usedFilteredImg[0].at<double>(i, j) != 0)
+                    {
+                        angle = atan( usedFilteredImg[1].at<double>(i, j) / usedFilteredImg[0].at<double>(i, j));
+                        angle *= 180 / M_PI;
+
+                        //if (angle > 90 )printf("angle %f \n", angle);
+                            pente.at<double>(i, j) = angle;
+                        }
+                        else if (usedFilteredImg[0].at<double>(i, j) == 0)
+                            angle = 0;
+
+                        pente.at<double>(i, j) = angle;
+                    }
+                    else
+                        pente.at<double>(i, j) = kmax * angle;
+                }
+            }
+            break;
+
+        default:
+            std::cout << "Mauvais mode de gradient" << std::endl;
+
+    }
+
+}
 
 string demanderImage()
 {
@@ -80,6 +235,8 @@ string demanderImage()
     images.push_back("tulipes.jpg (964*565)");
     images.push_back("Cartoon.jpg (1024*768)");
     images.push_back("arton553.jpg (1931*1931)");
+
+    images.push_back("lena.jpg ");
 
     images.push_back("MIF23/B.JPG ");
     images.push_back("MIF23/B1.JPG ");
@@ -109,89 +266,54 @@ string demanderImage()
             cout << tmp << endl << endl;
             return tmp;
         }
-        cout << "Le numero d'image choisit n'existe pas. " << tmp << " Reeseyez : " << endl;
+        cout << "Le numero d'image choisit n'existe pas. " << tmp << " Re-essayez : " << endl;
     }
 
 }
 
-cv::Vec3b filtreMatrice(cv::Mat* matImage, cv::Mat* matFiltre)
+cv::Mat applyFilter(cv::Mat& img, cv::Mat& filtre)
 {
-    double coef = 0;
+    cv::Mat m2(img.rows, img.cols, CV_64FC1);
 
-    double totalR = 0;
-    double totalV = 0;
-    double totalB = 0;
-
-    cv::Vec3b pix;
-
-    //parcourt les matrices 3x3 des deux images
-    for(int i=0; i<matImage->rows; i++)
+    //On parcours toute la matrice de l'image
+    for (int i = 0 ; i < img.rows ; i++)
     {
-        for(int j=0; j<matImage->cols; j++)
+        for (int j = 0 ; j < img.cols ; j++)
         {
-            if(matFiltre->at<double>(i,j) > 0)
-                coef = coef + matFiltre->at<double>(i,j);
-            totalR = totalR + matImage->at<cv::Vec3b>(i,j)[2] * matFiltre->at<double>(i,j);
-            totalV = totalV + matImage->at<cv::Vec3b>(i,j)[1] * matFiltre->at<double>(i,j);
-            totalB = totalB + matImage->at<cv::Vec3b>(i,j)[0] * matFiltre->at<double>(i,j);
-        }
-    }
+            int ki;
+            ki = i - filtre.rows/2;
+            int imin = (ki < 0)? 0 : ki;
+            ki = i + filtre.rows/2;
+            int imax = (ki > img.rows)? img.rows : ki;
 
-    if(coef !=0)
-    {
-        pix[2] = totalR/coef;
-        pix[1] = totalV/coef;
-        pix[0] = totalB/coef;
-    }
-    else
-    {
-        pix[2] = totalR;
-        pix[1] = totalV;
-        pix[0] = totalB;
-    }
-    //renvoie la couleur du pixel central
-    return(pix);
-}
+            int kj;
+            kj = j - filtre.cols/2;
+            int jmin = (kj < 0)? 0 : kj;
+            kj = j + filtre.cols/2;
+            int jmax = (kj > img.cols)? img.cols : kj;
 
-cv::Mat filtreImage(cv::Mat* imgSource, cv::Mat* filtre)
-{
-    cv::Range lignes;
-    cv::Range colonnes;
+            double sum = 0;
 
-    cv::Mat imgFiltre = imgSource->clone();
-    cv::Mat matImage;
+            ki = imin;
+            //On applique le filtre à l'image
+            while (ki <= imax)
+            {
+                kj = jmin;
+                while (kj <= jmax)
+                {
+                    int fi = ki - i + filtre.rows/2;
+                    int fj = kj - j + filtre.cols/2;
 
-    for(int i=1; i<(imgSource->rows-1); i++)
-    {
-        for(int j=1; j<(imgSource->cols-1); j++)
-        {
-            //récuprération de la matrice 3x3 autour du point de l'image courrante
-            lignes = cv::Range((i-1),(i+2));
-            colonnes = cv::Range((j-1),(j+2));
-            matImage = cv::Mat(imgFiltre, lignes, colonnes);
-            imgFiltre.at<cv::Vec3b>(i,j) = filtreMatrice(&matImage, filtre);
-        }
-    }
-    return imgFiltre;
-}
+                    uchar pix = img.at<uchar>(ki, kj);
+                    double coeff = filtre.at<double>(fi, fj);
+                    sum += pix * coeff;
 
-cv::Mat gris(cv::Mat* imgSource)
-{
-    cv::Mat imgFiltre = imgSource->clone();
-    int rouge, bleu, vert;
-
-    for(int i=0; i<(imgSource->rows); i++)
-    {
-        for(int j=0; j<(imgSource->cols); j++)
-        {
-            //récupération des couleurs
-            rouge = imgSource->at<cv::Vec3b>(i,j)[2];
-            bleu = imgSource->at<cv::Vec3b>(i,j)[0];
-            vert = imgSource->at<cv::Vec3b>(i,j)[1];
-            for(int k=0; k<3; k++){
-                imgFiltre.at<cv::Vec3b>(i,j)[k] = 0.2125*rouge + 0.0721*bleu + 0.7154*vert ;
+                    kj++;
+                }
+                ki++;
             }
+            m2.at<double>(i, j) = abs(sum);
         }
     }
-    return imgFiltre;
+    return m2;
 }
